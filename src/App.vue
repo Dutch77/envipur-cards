b9d01eeb8cd295a1ee909a3a73de641fb6797cec
MICHAL-PC\Michal <michalkolar.su@seznam.cz>
2019-08-06 21:44:27 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
+<style lang="scss">
+    /*body {*/
+    /*    h1 {*/
+    /*        color: red;*/
+    /*    }*/
+    /*}*/
+</style>
